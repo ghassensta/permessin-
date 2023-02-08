1d023b0f504d6dac36c88cff0db1fe005d1172f9
ghassensta <staghassen1@gmail.com>
2023-02-08 21:56:49 +0100

    laravel

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+
+@extends('layouts.app')
+
+
+
+<style>
+    .aaaa{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 20%;
+        width: 500px;
+        font-size: 2rem;
+    }
+    h3{
+        color: rgb(86, 86, 142);
+        font-family: cursive;
+    }
+    </style>
+@section('content')
+<div class="aaaa">
+
+    <h3>heloo in user interface</h3>
+
+</div>
+
+@endsection
